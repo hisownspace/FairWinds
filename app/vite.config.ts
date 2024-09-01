1c071ec237d71a1c6f05a78f3124cbbfce7f4085
David Nash <hisownspace@gmail.com>
2024-09-01 11:02:25 -0400

    edit vite.config.json for deployment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,20 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { qrcode } from "vite-plugin-qrcode";
 
 // https://vitejs.dev/config/
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    define: {
-      "process.env.VITE_GOOGLE_MAPS_API_KEY": JSON.stringify(
-        env.VITE_GOOGLE_MAPS_API_KEY,
-      ),
-      "process.env.VITE_GOOGLE_MAPS_MAP_ID": JSON.stringify(
-        env.VITE_GOOGLE_MAPS_API_KEY,
-      ),
-    },
-    plugins: [react(), qrcode()],
-  };
+export default defineConfig({
+  plugins: [react(), qrcode()],
 });
