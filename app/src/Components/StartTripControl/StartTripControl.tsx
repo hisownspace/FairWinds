b9213de458fa444b948d16eac98056df8c8ac733
David Nash <hisownspace@gmail.com>
2024-09-06 10:43:41 -0400

    add interface for starttripbutton props

--- app/src/Components/StartTripControl/StartTripControl.tsx
+++ app/src/Components/StartTripControl/StartTripControl.tsx
@@ -1,6 +1,14 @@
 import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ControlPosition, MapControl } from "@vis.gl/react-google-maps";
+import { SetStateAction } from "react";
+import { Dispatch } from "react";
+
+interface StartTripCtrlProps {
+  showStartTripButton: boolean;
+  onStartTrip: Dispatch<SetStateAction>;
+  tracking: boolean;
+}
 
 export default function StartTripControl({
   showStartTripButton,
